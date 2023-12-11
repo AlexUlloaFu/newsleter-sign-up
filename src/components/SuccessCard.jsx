c2d78a456535c7bd0c1b370ed5de8990cbdfcfe6
AlexUlloaFu <alexulloafu@gmail.com>
2023-12-11 00:04:37 -0500

    blue letter

--- src/components/SuccessCard.jsx
+++ src/components/SuccessCard.jsx
@@ -19,8 +19,8 @@ const SuccessCard = () => {
     return (
         <div className=" rounded-3xl shadow-2xl bg-White h-full w-full sm:h-[50%] sm:w-[50vh] p-10">
             <img className=" w-[17%] sm:-mt-2 mt-[15vh]" src={IconList} alt="succes icon"/>
-            <h1 className=" text-[6.2vh] font-Roboto font-semibold tracking-tight leading-none mb-4 mt-6">Thanks for subscribing!</h1>
-            <p className=" font-Roboto text-[2.2vh] sm:text-[1.6vh] mb-1 w-full ">A confirmation email has been sent to <span className=" font-semibold">{email}</span>. Please open it and click the button inside to confirm your subscription</p>
+            <h1 className=" text-[6.2vh] font-Roboto font-semibold tracking-tight leading-none mb-4 mt-6 text-DarkGrey">Thanks for subscribing!</h1>
+            <p className=" font-Roboto text-[2.2vh] sm:text-[1.6vh] mb-1 w-full text-DarkGrey">A confirmation email has been sent to <span className=" font-semibold">{email}</span>. Please open it and click the button inside to confirm your subscription</p>
             <button onClick={onDismissMessage} className=" font-Roboto text-center text-White bg-DarkGrey rounded-md w-full h-[7vh] sm:h-[15%] text-[1.7vh] mt-[27vh] sm:my-5 hover:bg-gradient-to-r from-Pink to-Orange">Dismiss message</button>
         </div>
     )
